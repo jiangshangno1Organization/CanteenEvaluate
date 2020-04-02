ce31a34dc02c2a85639afb3d2a8c3be3085f7baa
Kai <651535065@qq.com>
2020-04-02 16:17:29 +0800

    项目第一次搭建

---
+++ src/main/java/cn/resturant/util/DynamicDataSource.java
@@ -0,0 +1,11 @@
+package cn.resturant.util;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+
+public class DynamicDataSource extends AbstractRoutingDataSource {
+
+    protected Object determineCurrentLookupKey() {
+        return CustomerContextHolder.getCustomerType();
+    }
+}
